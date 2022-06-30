87aa7416d20e08c52afafc18a62677373039643c
inannan423 <605031236@qq.com>
2022-06-30 21:53:24 +0800

    sd
    但是出错了

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,20 @@
-
+module.exports = {
+    publicPath: './',
+    productionSourceMap: false,
+    //changeOrigin:true,
+    devServer: {
+        host: 'localhost',
+        port: 8200,
+        open: true,
+        proxy: {
+            //配置跨域
+            '/api': {
+                target: "localhost",
+                changOrigin: true,
+                pathRewrite: {
+                    '^/api': '/'
+                }
+            }
+        },
+    },
+}
